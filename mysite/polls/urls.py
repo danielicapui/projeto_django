486b82a760b71eaffb3953f9c03b8c9286795b33
danielicapui <danielicapui@users.noreply.github.com>
2021-10-24 12:30:47 -0300

    Parte 3 concluida

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 from . import views
+app_name ='polls'
 urlpatterns = [
+    #end:localhost/polls/
     path('', views.index, name='index'),
+    #end:/polls/5/
+    path('<int:question_id>/',views.detail,name='detail'),
+    #end:/polls/5/results/
+    path('<int:question_id>/results/',views.results,name='results'),
+    #end:/polls/5/vote/
+    path('<int:question_id>/vote/',views.vote,name='vote'),
+    path('<int:a>/<int:b>/<int:c>/raizes/',views.raizes,name='raizes'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse,render,get_object_or_404
+#from django.template import loader
+from .models import Question
 
 def index(request):
-    return HttpResponse("Bem vindo. Você está no index da polls!")
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    #template=loader.get_template('polls/index.html')
+    context={'latest_question_list': latest_question_list}
+    return render(request,'polls/index.html',context)
+def detail(request, question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})
+def results(request, question_id):
+    return HttpResponse("Você está olhando os resultados da questão {}.".format(question_id))
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão {}.".format(question_id))
+def raizes(request,a,b,c):
+    a,b,c=float(a),float(b),float(c)
+    x1=(-b+((b*b-4*a*c)**0.5))/2*a
+    x2=(-b-((b*b-4*a*c)**0.5))/2*a
+    return HttpResponse("O resultado da equação ({})x^2+({})x+({}) são ({},{})".format(a,b,c,x1,x2))
