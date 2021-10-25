c46504c7444cd606b9c734d126c4769bfdf9f975
danielicapui <danielicapui@users.noreply.github.com>
2021-10-25 12:15:32 -0300

    Atualizando views e vote

---
+++ mysite/polls/utills.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .models import Choice, Question
+
+def selectChoice(question):
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # mostrar o form atualizado
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # sempre retorna um HttpResponseRedirect depois de lidar com a solicitação
+        #previne envio repetido, caso o usuario retorne.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
